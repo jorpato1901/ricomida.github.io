bcb144185b93963ff495ae1b47ab95f6a1fc0a11
Jorge Villarroel <jorpato99@gmail.com>
2022-06-18 21:41:58 -0400

    Add files via upload

---
+++ Scritp/script.js
@@ -0,0 +1,27 @@
+console.log("Hola! estoy dentro del archivo script.js!!!!")
+
+$(function () {
+    console.log("Estoy dentro de la función autoejecutable de jquery")
+
+    //Aquí se colocaría la acción de "hide" del modal
+
+    $("#enviarCorreo").click(function (){
+        alert("El correo fue enviado correctamente...")
+
+        // Aquí se colocaría el "show" del modal
+    })
+
+    $(".card-title").click(function(){
+        $(".card-text").toggle()
+    })
+
+    $('body').on('dblclick','h4',function(){
+        $(this).css("color","red")
+    })
+
+
+
+
+})
+
+
